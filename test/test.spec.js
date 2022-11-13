00af343a56ebda32d5e6bc7a6bd6941f60e17d8f
yusuf963 <mohammad963yusuf@gmail.com>
2022-11-13 17:42:33 +0000

    make use of beforeeach method

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,17 +1,21 @@
 const assert = require('assert');
-describe('parent describe block',  ()=> {
+describe('parent describe block #1',  ()=> {
+  beforeEach(()=>{
+    console.log('before each')
+  })
   const value = 4
   const message = 'on the train'
   const message1 = true
   const message2 =0
   describe('#index1',  ()=> {
 
+
     it('should return -1 when the value is not present', ()=> {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
-    it('should fail', ()=>{
-      throw new Error('failed');
-    })
+    // it.skip('should fail', ()=>{
+    //   throw new Error('failed');
+    // })
     it('should pass', ()=>{
       assert.equal(value, 4);
     })
@@ -35,9 +39,9 @@ describe('parent describe block',  ()=> {
     it('should message be truthy value', ()=>{
       assert.ok(message1) // value should evaluate to true  and not any of false undefined null 0
     })
-    it('should match', ()=>{
-      assert.throws(()=>{})
-    })
+    // it.skip('should match', ()=>{
+    //   assert.throws(()=>{})
+    // })
 
 
   })
